b7512260e5e1f949d77683504755ea9159cb4ebb
Ji ZHANG <hope.zh@gmail.com>
2022-10-20 16:27:59 +0800

    add resize eventlistener to Sizes

--- src/Episode/Utils/Sizes.js
+++ src/Episode/Utils/Sizes.js
@@ -9,5 +9,12 @@ export default class Sizes
         this.width      = window.innerWidth;
         this.height     = window.innerHeight;
         this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+        // resize event
+        window.addEventListener('resize', () => {
+            this.width      = window.innerWidth;
+            this.height     = window.innerHeight;
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+        })
     }
 }
