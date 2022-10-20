789bf2ca351a987aa64cf97d2a872bbfa1f4a88c
Ji ZHANG <hope.zh@gmail.com>
2022-10-20 16:22:47 +0800

    add info to Sizes, change canvas bg color

--- src/Episode/Utils/Sizes.js
+++ src/Episode/Utils/Sizes.js
@@ -3,6 +3,11 @@ export default class Sizes
 {
     constructor ()
     {
-    
+        console.log('init Sizes')
+        
+        // setup 
+        this.width      = window.innerWidth;
+        this.height     = window.innerHeight;
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2);
     }
 }
