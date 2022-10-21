6ddcb932d39d6505cb52c2dbe4555fabe92368e1
Ji ZHANG <hope.zh@gmail.com>
2022-10-21 16:50:34 +0800

    add setInstance() to Camera

--- src/Episode/Camera.js
+++ src/Episode/Camera.js
@@ -1,12 +1,34 @@
 import * as THREE from "three";
-import Episode from './Episode.js';
+import Episode from "./Episode.js";
 
 // prettier-ignore
 export default class Camera
 {
-    constructor()
+    // [#] constructor
+    constructor()   
     {
+        // [+] options 
         this.episode = new Episode();
-        console.log(this.episode)
+        this.sizes   = this.episode.sizes
+        this.scene   = this.episode.scene
+        this.canvas  = this.episode.canvas
+
+        // [+] setup 
+        this.setInstance()
+    }
+
+    // [#] setInstance 
+    setInstance()
+    {
+        this.instance = new THREE.PerspectiveCamera(
+            35, 
+            this.sizes.width, 
+            this.sizes.height,
+            0.1, 
+            100
+        )
+
+        this.instance.position.set(6, 4, 8)
+        this.scene.add(this.instance)
     }
 }
